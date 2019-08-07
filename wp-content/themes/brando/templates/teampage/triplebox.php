d528a628c7b984f3c3831618381e1c13f56ebf73
df165 <davidbd@live.com>
2019-08-07 15:32:24 +0100

    Update triplebox.php

--- wp-content/themes/brando/templates/teampage/triplebox.php
+++ wp-content/themes/brando/templates/teampage/triplebox.php
@@ -4,7 +4,7 @@ $title       = get_sub_field('title');
 $dtitle       = get_sub_field('displayed_title');
 $sectionName = str_replace(' ', '', strtolower($title));
 ?>
-<section class="section casestudiesbgbottom  d-flex" data-section-name="<?php echo $sectionName; ?>">
+<section class="section casestudiesbgbottomop  d-flex" data-section-name="<?php echo $sectionName; ?>">
 <div class="container container--small align-self-center">
 
         <h2 class="main-title" style="color:white"><?php echo $dtitle?></h2>
