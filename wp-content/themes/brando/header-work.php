d6ad04fc8107a3b957c7fd9eb73b14c208056cac
df165 <davidbd@live.com>
2019-08-08 19:12:49 +0100

    css

--- wp-content/themes/brando/header-work.php
+++ wp-content/themes/brando/header-work.php
@@ -70,7 +70,7 @@
                     <div class="collapse navbar-collapse navbaritemz" id="navbarTogglerDemo02">
                         <ul class="navbar-nav mr-auto mt-2 mt-lg-0 navbarfr">
                         <li class="nav-item">
-                            <a style="color:white; font-weight:bold;" class="nav-link" href="../..">About <span class="sr-only">(current)</span></a>
+                            <a style="color:white; font-weight:bold;" class="nav-link" href="../..">Aboutfds <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
                             <a style="color:white; font-weight:bold;"  class="nav-link" href="../../Services">Services</a>
