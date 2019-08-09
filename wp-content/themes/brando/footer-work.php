f46477d2addaf40ecaca931c2db3a13dc82d3b76
df165 <davidbd@live.com>
2019-08-09 11:18:46 +0100

    Update footer-work.php

--- wp-content/themes/brando/footer-work.php
+++ wp-content/themes/brando/footer-work.php
@@ -130,15 +130,6 @@
             
 
             $(function() {
-                $(window).on('load', function(){
-                    $("[href]").each(function() {
-                        console.log(this.href);
-                        console.log(this);
-                        if (this.href == window.location.href) {
-                            $(this).addClass("active");
-                            console.log("hreffound");
-                        }
-                    });
                 <?php if(!is_singular('work')): ?>
                     $('#main-menu ul li a').on('click', function (e) {
                         var target = $(this).attr('href');
