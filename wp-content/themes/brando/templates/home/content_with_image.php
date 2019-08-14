791a44be39400a0621daf6e813f87eaaab9ee7ac
df165 <davidbd@live.com>
2019-08-14 16:49:07 +0100

    home page alignment

--- wp-content/themes/brando/templates/home/content_with_image.php
+++ wp-content/themes/brando/templates/home/content_with_image.php
@@ -11,7 +11,7 @@ $contentsmalltext  = get_sub_field('content_small_text');
 <div id="about">
     <div class="row" id="testidchange" style="color:#00498d;">
         <?php if($imagePos=='Left'): ?>
-            <div class="col-lg-6 text-center align-self-center left-content-work-block bgwidthcontent bg640" style="padding-left:0px; padding-right:0px; 
+            <div class="col-lg-5 text-center align-self-center left-content-work-block bgwidthcontent bg640" style="padding-left:0px; padding-right:0px; 
                     background-size: cover; background-position: center;height:100%; ">
                 <!--<?php if($image): ?>-->
                     <img class="servicescontentwithimageleft casestudieshomeimagewidth imagew100services hideservicesimagefront" src="<?php echo $image['url']; ?>">
@@ -19,8 +19,8 @@ $contentsmalltext  = get_sub_field('content_small_text');
                     <!--<img class="img-fluid imagefill content-image" src="<?php echo show_image($image['url'], $image['width'], $image['height']); ?>">-->
                 <!--<?php endif; ?>-->
             </div>
-            <div class="col-lg-6 align-self-center right-content-work-block casestudieshomeimagewidth img-order">
-                <div class="margin-2 padding-right" style="/*padding-top:40px;*/" >
+            <div class="col-lg-7 align-self-center right-content-work-block casestudieshomeimagewidth img-order">
+                <div class="padding240" style="/*padding-top:40px;*/" >
                 <img style="max-height:40px; max-width:40px;" class="img-fluid" src="<?php echo show_image($smallimage['url'], $smallimage['width'], $smallimage['height']); ?>">
                 <?php echo $title ? "<div class='content-title'>{$title}</div>" : '' ?>
                 <div class="contentimagep" style="font-size:24px;"><?php echo $content; ?></div>
@@ -28,15 +28,15 @@ $contentsmalltext  = get_sub_field('content_small_text');
             </div>
             </div>
         <?php elseif($imagePos=='Right'): ?>
-            <div class="col-lg-6 align-self-center left-content-work-block casestudieshomeimagewidth img-order" style="padding-left:70px;" >
-            <div class="margin-2 padding-right" style="/*padding-top:40px;*/" >
+            <div class="col-lg-7 align-self-center left-content-work-block casestudieshomeimagewidth img-order" style="padding-left:70px;" >
+            <div class="padding240" style="/*padding-top:40px;*/" >
             <img style="max-height:40px; max-width:40px;" class="img-fluid" src="<?php echo show_image($smallimage['url'], $smallimage['width'], $smallimage['height']); ?>">
                 <?php echo $title ? "<div class='content-title'>{$title} </div>" : '' ?>
                 <div class="contentimagep" style="font-size:24px;"> <?php echo $content; ?> </div>
                 <?php echo $contentsmalltext; ?>
             </div>
             </div>
-            <div class="col-lg-6 text-center align-self-center right-content-work-block bgwidthcontent bg640 " style="padding-left:0px; padding-right:0px;  
+            <div class="col-lg-5 text-center align-self-center right-content-work-block bgwidthcontent bg640 " style="padding-left:0px; padding-right:0px;  
                     background-size: cover; background-position: center;">
                 <?php if($image): ?>
                   <img class="servicescontentwithimageright casestudieshomeimagewidth imagew100services hideservicesimagefront" src="<?php echo $image['url']; ?>">
