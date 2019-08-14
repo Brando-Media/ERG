d582fd94164baa8cf1729bb6d4a97bd5a8d85158
df165 <davidbd@live.com>
2019-08-14 11:29:03 +0100

    Update front-page.php

--- wp-content/themes/brando/front-page.php
+++ wp-content/themes/brando/front-page.php
@@ -9,7 +9,7 @@ if($colour == "Blue"){
 }else{
     get_header(); 
 } ?>
-    <main><h1></h1>
+    <main>
     <section class="section  background-image-home" data-section-name="homes" style="max-height:750px; min-height:750px;">
     <div class="background-layer-home">
     <div class="frontpagetitle" style="
@@ -40,6 +40,7 @@ if($colour == "Blue"){
         }
     }
     ?> 
+    <br><br>  <br><br>  <br><br>  <br><br>  <br><br>  <br><br>  <br><br>
     </main>
 <?php if($colour == "Blue"){
     get_footer('project'); 
