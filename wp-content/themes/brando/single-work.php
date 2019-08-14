4b12ae74c01b408cb880422074f4bb3dfd04e0ed
df165 <davidbd@live.com>
2019-08-14 12:00:22 +0100

    Update single-work.php

--- wp-content/themes/brando/single-work.php
+++ wp-content/themes/brando/single-work.php
@@ -1,4 +1,5 @@
 <main>
+
 <?php 
 $completed = get_field('what_we_done');
 $colour = get_field('header_colour');
@@ -7,7 +8,6 @@ if($colour == "Blue"){
 }else{
     get_header('work'); 
 } ?>
-<main>
     <section class="section servicesoverviewbgright" style="min-height: 675px;">
         <div class="container container--small main-content serviceshometext">
             
@@ -43,7 +43,6 @@ if($colour == "Blue"){
         ?>
     <div class="section footerbg sectionservicefooter"  data-section-name="hello" style="min-height:20px;">    
     </div>
-    </main>
     <?php 
 if($colour == "Blue"){
     get_footer('project'); 
