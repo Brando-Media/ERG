c9ead763818fca1fe6e26e050c3f42c456ffa28a
df165 <davidbd@live.com>
2019-08-15 14:07:08 +0100

    ios hover

--- wp-content/themes/brando/templates/work/case_study_hover.php
+++ wp-content/themes/brando/templates/work/case_study_hover.php
@@ -4,7 +4,7 @@ $services    = get_sub_field('services');
 
 <section class=" white-bg " data-section-name="hover_images">
     <div style="";>
-        <div class="row services-experts" style="margin-left:0px; margin-right:0px; min-width:100vw;"> 
+        <div onclick="" class="row services-experts" style="margin-left:0px; margin-right:0px; min-width:100vw;"> 
             <div> <?php// var_dump($services); ?></div>
             <div class="col-lg-3 container imghoverw" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;" >
                 <?php //echo $services[0]['filter'] ?> 
@@ -14,7 +14,7 @@ $services    = get_sub_field('services');
                     <div class="viewcasestudy">VIEW CASE STUDY</div>
                 </div>
             </div>
-            <div class="col-lg-3 container imghoverw" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
+            <div onclick="" class="col-lg-3 container imghoverw" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
                 <?php //echo $services[1]['filter'] ?>  
                 <img class="img-fluid w-100" src="<?php echo show_image($services[1]['image']['url'], $services[1]['image']['width'], $services[1]['image']['height']) ?>"> 
                 <div class="overlay">
@@ -22,7 +22,7 @@ $services    = get_sub_field('services');
                     <div class="viewcasestudy">VIEW CASE STUDY</div>
                 </div>
             </div>
-            <div class="col-lg-3 container imghoverw" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
+            <div onclick="" class="col-lg-3 container imghoverw" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
             <?php //echo $services[2]['filter'] ?>  
             <img class="img-fluid w-100" src="<?php echo show_image($services[2]['image']['url'], $services[2]['image']['width'], $services[2]['image']['height']) ?>"> 
                 <div class="overlay">
@@ -30,7 +30,7 @@ $services    = get_sub_field('services');
                     <div class="viewcasestudy">VIEW CASE STUDY</div>
                 </div>
             </div>
-            <div class="col-lg-3 container imghoverw" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
+            <div onclick="" class="col-lg-3 container imghoverw" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
             <?php //echo $services[3]['filter'] ?>  
             <img class="img-fluid w-100" src="<?php echo show_image($services[3]['image']['url'], $services[3]['image']['width'], $services[3]['image']['height']) ?>"> 
                 <div class="overlay">
