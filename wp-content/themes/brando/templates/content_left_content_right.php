98b0dee5df7100afbf1e011a8e9c738d65d3d2ea
df165 <davidbd@live.com>
2019-08-15 10:10:46 +0100

    global alignment

--- wp-content/themes/brando/templates/content_left_content_right.php
+++ wp-content/themes/brando/templates/content_left_content_right.php
@@ -6,15 +6,15 @@ $ContentRight  = get_sub_field('content_right');
 ?>
 <section class="globalsection">
 <div class="container  container--small main-content align-self-center padleft25 globalpl0ml0 padleft25 ml0" style="padding-top:80px; margin-top:20px; min-width:310px;">
-        <div class="row d-flex" style="min-width:300px; padding-top:20px;">
-        <div class="col-lg-6 align-self-center left-content-work-block globalbtext globalcontent globalpl0ml0 nopl" style="margin-bottom:0px;">
+        <div class="pl16 row d-flex" style="min-width:300px; padding-top:20px;">
+        <div class="col-lg-6 align-self-center left-content-work-block globalbtext globalcontent globalpl0ml0 nopl ml0" style="margin-bottom:1px;">
             <?php echo $titleLeft ? "<h2 style='margin-bottom:20px; font-weight:400;'class='title globalptitle'>{$titleLeft}</h2>" : '' ?>
         </div>
         <div class="col-lg-6 align-self-center right-content-work-block globaltext globalcontent">
         </div>
     </div>
-    <div class="row d-flex" style='margin-bottom:20px;'>
-        <div class="col-lg-6 align-self-center left-content-work-block globalbtext globalcontent nopl">
+    <div class="pl16 row d-flex" style='margin-bottom:20px;'>
+        <div class="col-lg-6 align-self-center left-content-work-block globalbtext globalcontent nopl ml0">
             <?php echo $ContentLeft; ?>
         </div>
         <div class="col-lg-6 align-self-center right-content-work-block globaltext globalcontent">
