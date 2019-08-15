d65c59ab114ae99a6463d3160713096ceba7be4e
df165 <davidbd@live.com>
2019-08-15 14:17:07 +0100

    home pad right

--- wp-content/themes/brando/templates/work/services_overview.php
+++ wp-content/themes/brando/templates/work/services_overview.php
@@ -28,14 +28,14 @@ $linkurl       = get_sub_field('link_url');
                         <?php } ?>
                         <img class="img-fluid " style="margin-bottom:20px; max-width:60%!important; top:54px; position: relative; margin-left: auto;margin-right: auto;" src="<?php echo show_image($servicerow['image']['url'], $servicerow['image']['width'], $servicerow['image']['height'],0.75) ?>"> 
                         </div>
-                        <div class="servicescontent nomarginl991 nomarginr991 padright99110">
+                        <div class="servicescontent nomarginl991 nomarginr991 padright99110 maxwidth991100">
                         <div class="servicesubtitle" style="color:white;"><?php echo $servicerow['title']; ?></div>
                         <?php echo $servicerow['content']; ?>
                         </div>
                     </div>
                 <?php }else if($servicerow['text_location']=="Top"){ ?>
                     <div class="col-lg-3" style="padding-left:0px; padding-right:0px;">
-                        <div class="servicescontent nomarginl991 nomarginr991 padright99110" style=""><div class="servicesubtitle" style="color:white;"><?php echo $servicerow['title']; ?></div>
+                        <div class="servicescontent nomarginl991 nomarginr991 padright99110 maxwidth991100" style=""><div class="servicesubtitle" style="color:white;"><?php echo $servicerow['title']; ?></div>
                         <?php echo $servicerow['content']; ?></div>
                         <?php if($first != 0){ ?>
                         <div class="servicesimg" style="text-align:center;">
