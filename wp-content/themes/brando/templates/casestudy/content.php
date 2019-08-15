e6d92ff8da38d236b155464ed0d2fc3178af0b3e
df165 <davidbd@live.com>
2019-08-15 13:40:07 +0100

    team pages

--- wp-content/themes/brando/templates/casestudy/content.php
+++ wp-content/themes/brando/templates/casestudy/content.php
@@ -9,11 +9,11 @@ $sectionName = str_replace(' ', '', strtolower($title));
         <div class="container container--small main-content serviceshometext" style="padding-bottom:0px;">
             
             <div class="row">
-                <div class="col-lg-12 projects-home" style="color:white;">
+                <div class="ml10 col-lg-12 projects-home" style="color:white;">
                     <div class=" left-content-teampage">
-                        <div class="fontworksingle" style="padding-left:15px; margin-bottom:30px;">
+                        <div class="fontworksingle" style="margin-bottom:30px;">
                         <?php the_post(); the_title(); ?> </div>
-                        <div class="col-lg-6 work-completed strongwordpress" style="max-width:700px;">
+                        <div class="col-lg-6 work-completed strongwordpress" style="max-width:700px; padding-left:0px;">
                         <?php echo $content ?>
                         </div>
                     </div>
