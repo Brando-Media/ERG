22bb8587386cf71d33d22e4c562275d9be89be40
Illya <davidbd@live.com>
2019-09-14 15:16:43 +0100

    css

--- wp-content/themes/brando/templates/project/project_home.php
+++ wp-content/themes/brando/templates/project/project_home.php
@@ -50,7 +50,7 @@ $next_url = get_sub_field('nexturl');
 <div class="row d-flex indicasepad marginautoindicase" id="marginautoindicase" style="margin-top:50px;">
         <?php if(!empty($previoustext)){ ?> 
             <div class="indicasenavtext col-lg-6 case-prev-container" style="" >
-            <div class="case-prev-text" style=" text-align:left;">
+            <div class="proj-prev-text" style=" text-align:left;">
                 <a style="font-family: Helvetica;color: #00498D!important; text-align: left;" href="<?php echo $previousurl ?>"><div class="leftindipad" id="leftcasestudyindi" style="text-align: left; padding-left:0px;"> <img src="../../wp-content/themes/brando/assets/images/rightah.png"><?php echo $previoustext ?></div></a>
             </div>
         </div>
