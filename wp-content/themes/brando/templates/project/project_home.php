9c512f8ae876c368dbbd503fdc2cd99ea3f11bda
Illya <davidbd@live.com>
2019-09-14 13:28:21 +0100

    arrow changes

--- wp-content/themes/brando/templates/project/project_home.php
+++ wp-content/themes/brando/templates/project/project_home.php
@@ -44,15 +44,19 @@ $next_url = get_sub_field('nexturl');
         <?php endif; ?>
     </div>
 </div>
-<div class="row d-flex casenavhide indicasepad marginautoindicase" id="marginautoindicase">
+<div class="row d-flex indicasepad marginautoindicase" id="marginautoindicase" style="margin-top:50px;">
         <?php if(!empty($previoustext)){ ?> 
-            <div class="indicasenavtext" style="width:50vw;" >
-                <a style="font-family: Helvetica;color: #00498D!important;" href="<?php echo $previous_url ?>"><div class="leftindipad" id="leftprojectindi"> <img src="../../wp-content/themes/brando/assets/images/rightah.png"><?php echo $previoustext ?></div></a>
+            <div class="indicasenavtext col-lg-6 case-prev-container" style="" >
+            <div class="case-prev-text" style=" text-align:left;">
+                <a style="font-family: Helvetica;color: #00498D!important; text-align: left;" href="<?php echo $previous_url ?>"><div class="leftindipad" id="leftcasestudyindi" style="text-align: left; padding-left:0px;"> <img src="../../wp-content/themes/brando/assets/images/rightah.png"><?php echo $previoustext ?></div></a>
             </div>
+        </div>
         <?php } ?>
         <?php if(!empty($nexttext)){ ?>
-            <div id="" class="indicasenavtext" style="width:50vw;" >
-            <a style="font-family: Helvetica; color: #00498D!important;" href="<?php echo $next_url ?>"><div class="rightindipad" id="rightprojectindi"> <?php echo $nexttext ?><img src="../../wp-content/themes/brando/assets/images/leftah.png"></div> </a>
+            <div id="" class="indicasenavtext col-lg-5 case-next-container" style="" >
+            <div style=" text-align:right;" class="case-next-text">
+            <a style="font-family: Helvetica; color: #00498D!important;" href="<?php echo $next_url ?>"><div class="rightindipad" id="rightcasestudyindi" style="text-align: right; padding-right:0px;"> <?php echo $nexttext ?><img src="../../wp-content/themes/brando/assets/images/leftah.png"></div> </a>
+            </div>
             </div>
         <?php } ?>
         </div>
