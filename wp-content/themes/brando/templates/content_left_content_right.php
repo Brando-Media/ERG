7886546930d01b1e3e5fe4574c98e478a1fe0d9e
df165 <davidbd@live.com>
2019-09-17 15:03:47 +0100

    remove align self center

--- wp-content/themes/brando/templates/content_left_content_right.php
+++ wp-content/themes/brando/templates/content_left_content_right.php
@@ -7,7 +7,7 @@ $ContentRight  = get_sub_field('content_right');
 <section class="globalsection">
 <div class="maxwidth991100 container  container--small main-content align-self-center padleft25 globalpl0ml0 padleft25 ml0 nomarginl991" style="padding-top:80px; margin-top:20px; min-width:310px;">
         <div class="pl12 row d-flex" style="min-width:300px; padding-top:20px;">
-        <div class="col-lg-6 align-self-center left-content-work-block globalbtext globalcontent globalpl0ml0 nopl ml0" style="margin-bottom:1px;padding-right:0px;">
+        <div class="col-lg-6 left-content-work-block globalbtext globalcontent globalpl0ml0 nopl ml0" style="margin-bottom:1px;padding-right:0px;">
             <?php echo $titleLeft ? "<h2 style='margin-bottom:20px; font-weight:400;'class='title globalptitle'>{$titleLeft}</h2>" : '' ?>
         </div>
         <div class="col-lg-6 align-self-center right-content-work-block globaltext globalcontent">
