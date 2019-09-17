4f03b35e0d4a4899218c89f3562c93d9f1c78a04
df165 <davidbd@live.com>
2019-09-17 14:59:55 +0100

    css

--- wp-content/themes/brando/templates/content_left_content_right.php
+++ wp-content/themes/brando/templates/content_left_content_right.php
@@ -17,7 +17,7 @@ $ContentRight  = get_sub_field('content_right');
         <div class="col-lg-6 align-self-center left-content-work-block globalbtext globalcontent nopl ml0" style="padding-right:0px;">
             <?php if(!empty($ContentLeft)){ echo $ContentLeft; } ?>
         </div>
-        <div class="col-lg-6 align-self-center right-content-work-block globaltext globalcontent nopl" style="padding-right:0px;">
+        <div class="col-lg-6 align-self-center right-content-work-block globaltext globalcontent nopl globalstyle" style="padding-right:0px;">
             <?php echo $ContentRight ?>
         </div>
     </div>
