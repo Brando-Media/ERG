e5b7ce66e19c89aded72686a3d618583fd216ae0
df165 <davidbd@live.com>
2019-09-30 15:24:50 +0100

    css

--- wp-content/themes/brando/templates/whatwedo/content_with_image.php
+++ wp-content/themes/brando/templates/whatwedo/content_with_image.php
@@ -7,11 +7,11 @@ $content  = get_sub_field('content');
 $contentsmalltext  = get_sub_field('content_small_text');
 ?>
 
-<section>
+<section> 
 <div id="about">
     <div class="row" id="testidchange" style="color:#00498d;">
         <?php if($imagePos=='Left'): ?>
-            <div class="col-lg-6 text-center align-self-center left-content-work-block  /*bgwidthcontent bg640*/" style="padding-left:0px; padding-right:0px; 
+            <div class="col-lg-6 wwd1600 text-center align-self-center left-content-work-block  /*bgwidthcontent bg640*/" style="padding-left:0px; padding-right:0px; 
                     background-size: cover; background-position: center;height:100%; ">
                 <!--<?php if($image): ?>-->
                     <img style="height:auto;" class="servicescontentwithimageleft  imagew100services img100" src="<?php echo $image['url']; ?>">
@@ -19,8 +19,8 @@ $contentsmalltext  = get_sub_field('content_small_text');
                     <--<img class="img-fluid imagefill content-image" src="<?php echo show_image($image['url'], $image['width'], $image['height']); ?>">-->
                 <!--<?php endif; ?>-->
             </div>
-            <div class="col-lg-6 align-self-center right-content-work-block  img-order">
-                <div class="margin-2 padding-right" style="/*padding-top:40px;*/" >
+            <div class="col-lg-6 wwd1600 align-self-center right-content-work-block  img-order">
+                <div class="margin-5wwd padding-right" style="/*padding-top:40px;*/" >
                 <img style="max-height:40px; max-width:40px;" class="img-fluid" src="<?php echo show_image($smallimage['url'], $smallimage['width'], $smallimage['height']); ?>">
                 <?php echo $title ? "<div class='content-title'>{$title}</div>" : '' ?>
                 <div class="contentimagep" style="font-size:24px;"><?php echo $content; ?></div>
@@ -28,15 +28,15 @@ $contentsmalltext  = get_sub_field('content_small_text');
             </div>
             </div>
         <?php elseif($imagePos=='Right'): ?>
-            <div class="col-lg-6 align-self-center left-content-work-block  img-order" style="padding-left:70px;" >
-            <div class="margin-2 padding-right" style="/*padding-top:40px;*/" >
+            <div class="col-lg-6 wwd1600 align-self-center left-content-work-block  img-order" style="padding-left:70px;" >
+            <div class="margin-5wwd padding-right" style="/*padding-top:40px;*/" >
             <img style="max-height:40px; max-width:40px;" class="img-fluid" src="<?php echo show_image($smallimage['url'], $smallimage['width'], $smallimage['height']); ?>">
                 <?php echo $title ? "<div class='content-title'>{$title} </div>" : '' ?>
                 <div class="contentimagep" style="font-size:24px;"> <?php echo $content; ?> </div>
                 <?php echo $contentsmalltext; ?>
             </div>
             </div>
-            <div class="col-lg-6 text-center align-self-center right-content-work-block  /*bgwidthcontent bg640*/" style="padding-left:0px; padding-right:0px;  
+            <div class="col-lg-6 wwd1600 text-center align-self-center right-content-work-block  /*bgwidthcontent bg640*/" style="padding-left:0px; padding-right:0px;  
                     background-size: cover; background-position: center;">
                 <?php if($image): ?>
                   <img style="height:auto;" class="servicescontentwithimageright  imagew100services img100" src="<?php echo $image['url']; ?>">
