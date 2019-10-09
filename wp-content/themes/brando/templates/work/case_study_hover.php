bbe11ed20c852dd27cff22b61f74777eaba98cdf
df165 <davidbd@live.com>
2019-10-09 17:17:28 +0100

    css changes

--- wp-content/themes/brando/templates/work/case_study_hover.php
+++ wp-content/themes/brando/templates/work/case_study_hover.php
@@ -6,7 +6,7 @@ $services    = get_sub_field('services');
     <div style="";>
         <div onclick="" class="row services-experts" style="margin-left:0px; margin-right:0px; min-width:100vw;"> 
             <div> <?php// var_dump($services); ?></div>
-            <div class="col-lg-3 container imghoverw hoverimg" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;" >
+            <div class="col-lg-3 container imghoverw homeimg284" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;" >
                <!-- <a href="<?php echo $services[0]['url']; ?>">-->
                 <?php //echo $services[0]['filter'] ?> 
                 <img class="img-fluid w-100" src="<?php echo show_image($services[0]['image']['url'], $services[0]['image']['width'], $services[0]['image']['height'],0.75) ?>"> 
@@ -16,7 +16,7 @@ $services    = get_sub_field('services');
                 </div>-->
                 </a>
             </div>
-            <div onclick="" class="col-lg-3 container imghoverw hoverimg" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
+            <div onclick="" class="col-lg-3 container imghoverw homeimg284" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
                  <!-- <a href="<?php echo $services[1]['url']; ?>">-->
                 <?php //echo $services[1]['filter'] ?>  
                 <img class="img-fluid w-100" src="<?php echo show_image($services[1]['image']['url'], $services[1]['image']['width'], $services[1]['image']['height']) ?>"> 
@@ -26,7 +26,7 @@ $services    = get_sub_field('services');
                 </div>-->
                 </a>
             </div>
-            <div onclick="" class="col-lg-3 container imghoverw hoverimg" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
+            <div onclick="" class="col-lg-3 container imghoverw homeimg147" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
              <!-- <a href="<?php echo $services[2]['url']; ?>">-->
             <?php //echo $services[2]['filter'] ?>  
             <img class="img-fluid w-100" src="<?php echo show_image($services[2]['image']['url'], $services[2]['image']['width'], $services[2]['image']['height']) ?>"> 
@@ -36,7 +36,7 @@ $services    = get_sub_field('services');
                 </div>-->
                 </a>
             </div>
-            <div onclick="" class="col-lg-3 container imghoverw hoverimg" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
+            <div onclick="" class="col-lg-3 container imghoverw homeimg284" style="padding-left:0px; padding-right:0px; margin-left:0px; margin-right:0px;">
              <!-- <a href="<?php echo $services[3]['url']; ?>">-->
             <?php //echo $services[3]['filter'] ?>  
             <img class="img-fluid w-100" src="<?php echo show_image($services[3]['image']['url'], $services[3]['image']['width'], $services[3]['image']['height']) ?>"> 
