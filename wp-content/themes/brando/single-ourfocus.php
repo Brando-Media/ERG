a6308748f124730af1170b2a6a4f53514bccf0fd
df165 <davidbd@live.com>
2019-10-10 16:19:31 +0100

    ratm

--- wp-content/themes/brando/single-ourfocus.php
+++ wp-content/themes/brando/single-ourfocus.php
@@ -13,22 +13,7 @@ if($colour == "Blue"){
 } ?>
     <main class="mainstyle">
 
-    <section class="section servicesoverviewbgright" style="min-height: 675px;">
-        <div class="container container--small main-content serviceshometext">
-            <div class="row">
-                <div class="col-lg-12 projects-home" style="color:white; padding-left:0px;">
-                    <div  style="/*max-width:90%;*/"-->
-                        <div class="worktitlec" style="color:white; font-size: 50px;margin-bottom: 30px;padding-left:15px;font-weight: 300; font-family: 'PangramBlack';font-style: normal;">
-                        <div class="fontworksingle d" style="padding-left:15px; margin-bottom:30px;"><?php the_post(); the_title(); ?></div>
-                        <div class="col-lg-12  work-completed" style="max-width:700px;"><?php the_content(); ?></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6 work-completed">
-                </div>
-            </div>
-        </div>
-        </section>
+   
         <?php 
         if(have_rows('content')){
             while(have_rows('content')){
@@ -39,7 +24,7 @@ if($colour == "Blue"){
                 if(get_row_layout() == 'content_with_image_at_top') get_template_part('templates/work/content_with_image_at_top');
                 if(get_row_layout() == 'content_left_content_right') get_template_part('templates/work/content_left_content_right');
                 if(get_row_layout() == 'imagenobotpadding') get_template_part('templates/work/imagenobotpadding');
-                if(get_row_layout() == 'project_filter') get_template_part('templates/work/project_filter');
+                if(get_row_layout() == 'project_filter') get_template_part('templates/ourfocus/project_filter');
                 if(get_row_layout() == 'two_images_content') get_template_part('templates/casestudy/two_images_content'); 
                 if(get_row_layout() == 'career_content') get_template_part('templates/work/career_content'); 
 
---
+++ wp-content/themes/brando/templates/ourfocus/project_filter.php
@@ -0,0 +1,45 @@
+<?php 
+$image    = get_sub_field('image');
+$services = get_sub_field('services');
+$first = 0;
+?>
+ <section class="section servicesoverviewbgright" style="min-height: 100vh;">
+        <div class="container container--small main-content serviceshometext">
+            <div class="row">
+                <div class="col-lg-12 projects-home" style="color:white; padding-left:0px;">
+                <div class="">
+                    <div class="row" style="color:#fff;">
+                            
+                            <div class="col-lg-12 right-content-work-block filter-padding pfcol6">
+                            <?php foreach($services as $servicerow){ 
+                                if($first==0){?>
+                                    <button class="accordionproject" style="border-top: 1px solid #fff; outline: none!Important;"><?php echo $servicerow['project_title']?></button>
+                                    <div class="panelproject">
+                                    <?php foreach($servicerow as $projectrow){ ?>
+                                        <?php foreach($projectrow as $row){ ?>
+                                            <div style="padding-bottom:25px;"> <a class="hoverremoveactive" style="color: #fff!important;" onMouseOver="this.style.color='#fff';" onMouseOut="this.style.color='#fff'" href="<?php echo $row['project_url'] ?>"><?php echo $row['project_title'] ?></a> </div>
+                                        <?php } ?>
+                                    <?php } ?>
+                                    </div>
+                                <?php }else{?>
+                                    <button class="accordionproject" style="border-top: 1px solid #fff; outline: none!Important;"><?php echo $servicerow['project_title']?></button>
+                                    <div class="panelproject">
+                                    <?php foreach($servicerow as $projectrow){ ?>
+                                        <?php foreach($projectrow as $row){ ?>
+                                            <div style="padding-bottom:25px;"> <a class="hoverremoveactive"  style="color: #fff!important;" onMouseOver="this.style.color='#fff';" onMouseOut="this.style.color='#fff'" href="<?php echo $row['project_url'] ?>"><?php echo $row['project_title'] ?></a> </div>
+                                        <?php } ?>
+                                    <?php } ?>
+                                    </div>
+                                    <!--<div class="col-lg-6 text-center /*align-self-center*/ left-content-work-block pfcol6" style="padding-left:0px; padding-right:0px;">
+                            </div>-->
+                            <?php } $first++; ?>
+                            <?php }?>
+                            <div style="min-width:150px; height:2px;border-top: 1px solid #fff;"></div>
+                            </div>
+                            
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        </section>
