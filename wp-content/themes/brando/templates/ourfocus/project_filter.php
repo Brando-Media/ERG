fe51cb2a769402e6af01d42e673d9ad279af24e0
df165 <davidbd@live.com>
2019-10-10 17:16:27 +0100

    php changes

--- wp-content/themes/brando/templates/ourfocus/project_filter.php
+++ wp-content/themes/brando/templates/ourfocus/project_filter.php
@@ -1,6 +1,7 @@
 <?php 
 $image    = get_sub_field('image');
 $services = get_sub_field('services');
+$title = get_sub_field('title');
 $first = 0;
 ?>
  <section class="section servicesoverviewbgright" style="min-height: 100vh;">
@@ -11,7 +12,7 @@ $first = 0;
                     <div class="row" style="color:#fff;">
                             
                             <div class="col-lg-12 right-content-work-block pfcol6" style="max-width:700px;">
-                            <div class="ourfocustext"><?php echo $servicerow['title']?></div>
+                            <div class="ourfocustext"><?php echo $title ?></div>
                             <?php foreach($services as $servicerow){ 
                                 if($first==0){?>
                                     <button class="accordionproject" style="border-top: 1px solid #fff; outline: none!Important; position:relative;"><span class="ourfocustextsize" style="font-size:24px;"><?php echo $servicerow['project_title']?></span></button>
