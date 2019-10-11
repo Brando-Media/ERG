f5117bd00a8879a25f8eb963476b17abe5a7a18a
df165 <davidbd@live.com>
2019-10-11 10:35:26 +0100

    test

--- wp-content/themes/brando/templates/allstudy/projecthome.php
+++ wp-content/themes/brando/templates/allstudy/projecthome.php
@@ -83,7 +83,7 @@ $nexttext = get_sub_field('next_text');
 $nexturl = get_sub_field('next_url');
 
 ?>
-<section class="">
+<section class="" style="position:relative;">
 
 <div class="">
     <div class="row d-flex" style="color:#00498d;">
@@ -123,95 +123,28 @@ $nexturl = get_sub_field('next_url');
         <?php } ?>
         </div>
 
-        <header id="" class="arrowpos" style="">
-            
-                <div id="top" class="container clearfix header-toggle-white header-div" style="margin-left:60px; max-width:2400px; padding-left:0px; padding-right:0px;">
-                    <a href="#home" id="logo" class="float-left" style="padding-left:15px;">
-                        <img style="max-width:100px; margin-top:6px;" class="white-logo" src="<?php bloginfo('template_url') ?>/assets/images/ERG-logo.png" alt="erg" title="Brando Media" />
-                        <img class="red-logo" src="<?php bloginfo('template_url') ?>/assets/images/ERG-logo.png" alt="erg" title="Brando Media" />
-                    </a>
-                    <!--<nav class="navbar navbar-expand-lg navbar-light navbar-brand" >
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                        <a style="color:white;" class="nav-item nav-link active hover-underline-link about-link" href="#">Home</a>
-                        <a style="color:white;"  class="nav-item nav-link hover-underline-link about-link" href="#">Features</a>
-                        <a style="color:white;" class="nav-item nav-link hover-underline-link about-link" href="#">Pricing</a>
-                        </div>
-                    </div>
-                    </nav> -->
-                    <nav class="navbar navbar-expand-lg navfloat" id="navfloatx">
-                    <a class="navbar-brand" href="#"></a>
-                    <button id="navbar-header-toggle" onclick="togglemob();" class="navbar-toggler collapsed">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    </nav>
-                    
-                   <!-- <nav class="navbar navbar-expand-lg navfloat" id="navfloatx">
-                    <a class="navbar-brand" href="#"></a>
-                    <button id="navbar-header-toggle" onclick="togglemob();" class="navbar-toggler collapsed">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>-->
+        <div id="wrapper" class="<?php echo $class; ?>">
+   
+   <header id="" class="arrowpos" style="">
+   
+       <div id="top" class="container clearfix header-toggle-white header-div" style="margin-left:60px; max-width:2400px; padding-left:0px; padding-right:0px;">
+           <a href="#home" id="logo" class="float-left" style="padding-left:15px;">
+               
+           <a style="font-family: Helvetica;color: #00498D!important; text-align: left;" href="<?php echo $previousurl ?>"><div class="leftindipad" id="leftcasestudyindi" style="text-align: left; padding-left:0px;"> <img class="leftarrowmargin" src="../wp-content/themes/brando/assets/images/rightah.png"><?php echo $previoustext ?></div></a>
+           </a>
 
-                    <div class="collapse navbar-collapse navbaritemz" id="navbarTogglerDemo02" style="min-width:100vw; float:left">
-                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0 navbarfr">
-                        <li class="nav-item">
-                            <a style="color:white; font-weight:bold;"  class="nav-link navhover" onclick="hrefMove('about')" href="https://brando.media/erg/#about/" >About <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white; font-weight:bold;" class="nav-link navhover" href="erg/what-we-do">What We Do</a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white; font-weight:bold;" class="nav-link navhover" href="ourfocus">Our Focus</a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white; font-weight:bold;" class="nav-link navhover" href="case-studies">Case Studies</a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white;font-weight:bold;" class="nav-link navhover" onclick="hrefMove('global')" href="https://brando.media/erg/#global/"  >Global</a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white;font-weight:bold;" class="nav-link navhover" href="team-pages">Team</a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white;font-weight:bold;" class="nav-link navhover" onclick="hrefMove('contact')" href="https://brando.media/erg/#contact/"  >Contact</a>
-                        </li>
-                        </ul>
-                    </div>
-              <!--      </nav> -->
 
-              <nav class="navbar navbar-expand-lg navfloat" id="navfloatx">
-                    <div class="collapse navbar-collapse navbaritemz" id="navbarTogglerDemo02">
-                        <ul class="navbar-nav mr-auto mt-2 mt-lg-0 navbarfr">
-                        <li class="nav-item">
-                            <a style="color:white; font-weight:bold;" class="nav-link navhover" id="aboutnav" onclick="changeNavCol('aboutnav')" href="https://brando.media/erg/#about">About <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white; font-weight:bold;" class="nav-link navhover" href="what-we-do">What We Do</a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white; font-weight:bold;" class="nav-link navhover" href="ourfocus">Our Focus</a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white; font-weight:bold;" class="nav-link navhover" href="case-studies">Case Studies</a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white;font-weight:bold;" class="nav-link navhover" id="globalnav" onclick="changeNavCol('globalnav')" href="https://brando.media/erg/#global">Global</a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white;font-weight:bold;" class="nav-link navhover" href="team-pages">Team</a>
-                        </li>
-                        <li class="nav-item">
-                            <a style="color:white;font-weight:bold;" class="nav-link navhover" id="contactnav" onclick="changeNavCol('contactnav')" href="https://brando.media/erg/#contact">Contact</a>
-                        </li>
-                        </ul>
-                    </div>
-                    </nav>
 
-                </div>
-            </header>
+
+            <nav class="navbar navbar-expand-lg navfloat" id="navfloatx">
+           sadasd
+            </nav>
+
+       </div>
+   </header>
+
+
+
 
 <!--        
 <div class="row project-home-footer col-lg-12" style="display: -webkit-box; margin:auto; margin-top:10%; margin-bottom:10%;">
