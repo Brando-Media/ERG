736e16474d8450ba0501d6c3ded02acba1549ae3
df165 <davidbd@live.com>
2019-10-16 12:08:34 +0100

    imagesfourt

--- wp-content/themes/brando/single-project.php
+++ wp-content/themes/brando/single-project.php
@@ -33,6 +33,8 @@ if($colour == "Blue"){
                 if(get_row_layout() == 'image') get_template_part('templates/project/image');
                 if(get_row_layout() == 'content_with_image') get_template_part('templates/project/content_with_image');
                 if(get_row_layout() == 'images') get_template_part('templates/project/images'); 
+                if(get_row_layout() == 'images_three') get_template_part('templates/project/imagestriple'); 
+                if(get_row_layout() == 'images_four') get_template_part('templates/project/imagesfour'); 
                 if(get_row_layout() == 'content_with_image_at_top') get_template_part('templates/project/content_with_image_at_top');
                 if(get_row_layout() == 'content_left_content_right') get_template_part('templates/project/content_left_content_right');
                 if(get_row_layout() == 'imagenobotpadding') get_template_part('templates/project/imagenobotpadding');
@@ -40,7 +42,7 @@ if($colour == "Blue"){
                 if(get_row_layout() == 'project_home') get_template_part('templates/project/project_home');
             }
         }
-        ?>
+        ?> 
         <div class="section footerbg"  data-section-name="hello" style="min-height:20px;">    
         </div>
     </main>
---
+++ wp-content/themes/brando/templates/project/imagessfour.php
@@ -0,0 +1,18 @@
+<?php 
+$images = get_sub_field('images'); 
+?>
+
+<?php if($images): ?>
+    <div class="row m-0">
+        <div class="col-md-12 mw69div" style="padding-left:0px!important; padding-right:0px!important;">
+        <div class="row m-0">
+        <?php foreach($images as $k=>$image): ?>
+           <div class="<?php echo $class ?>" style="min-width:50vw;">
+           <div class="<?php echo $class ?> col-md-12 container  casestudy-div-img maxwidth23 from50to100p <?php echo $class ?>" style="padding-left:0px!important; padding-right:0px!important; margin-left:0px!important; margin-right:0px!important; width:100%;" >
+                <img class="small-project-homep maxwidth23 from50to100p" src="<?php echo show_image($image['url'], $image['width'], $image['height'], 75) ?>">
+            </div>
+            </div>
+        <?php endforeach; ?>
+            </div>
+            </div>
+<?php endif; ?>
