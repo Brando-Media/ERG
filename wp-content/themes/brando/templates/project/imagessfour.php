df825d22c6f605f88ae289fc0389ee389b29eae8
df165 <davidbd@live.com>
2019-10-16 12:27:12 +0100

    triple css

--- wp-content/themes/brando/templates/project/imagessfour.php
+++ wp-content/themes/brando/templates/project/imagessfour.php
@@ -9,7 +9,7 @@ $images = get_sub_field('images');
         <?php foreach($images as $k=>$image): ?>
            <div class="<?php echo $class ?>" style="min-width:50vw;">
            <div class="<?php echo $class ?> col-md-12 container  casestudy-div-img maxwidth23 from50to100p <?php echo $class ?>" style="padding-left:0px!important; padding-right:0px!important; margin-left:0px!important; margin-right:0px!important; width:100%;" >
-                <img class="small-project-homep maxwidth23 from50to100p" src="<?php echo show_image($image['url'], $image['width'], $image['height'], 75) ?>">
+                <img class="maxwidth23 from50to100p" src="<?php echo show_image($image['url'], $image['width'], $image['height'], 75) ?>">
             </div>
             </div>
         <?php endforeach; ?>
