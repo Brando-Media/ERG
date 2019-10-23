60c3d071d6c48d411c0c145c435f9a3b119db737
df165 <davidbd@live.com>
2019-10-23 12:08:03 +0100

    Update projecthome.php

--- wp-content/themes/brando/templates/teampage/projecthome.php
+++ wp-content/themes/brando/templates/teampage/projecthome.php
@@ -25,7 +25,7 @@ $projectitle = get_sub_field('project_title');
                 </div>
                 
             </div>
-            <div class="col-lg-5 project-home-text pt210m0 pb90m50 inditeamtext ftmsection resize-text M90L" style="margin-top:0px; padding-left:90px; padding-right:15px;max-width: 550px;">  
+            <div class="col-lg-5 project-home-text pt210m0 /*pb90m50*/ inditeamtext ftmsection resize-text M90L" style="margin-top:0px; padding-left:90px; padding-right:15px;max-width: 550px;">  
                 <?php echo $title ? "<div class='inditeamtitle'>{$title}</div>" : '' ?>
                 <div class="teampageind-title">
                 <?php echo $projectitle ? "{$projectitle}" : '' ?>
