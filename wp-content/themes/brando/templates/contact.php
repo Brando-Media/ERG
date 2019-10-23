a607faaf15a3b5d7927eebd9cbe5f2b5737454bc
df165 <davidbd@live.com>
2019-10-23 10:42:30 +0100

    Update contact.php

--- wp-content/themes/brando/templates/contact.php
+++ wp-content/themes/brando/templates/contact.php
@@ -31,12 +31,12 @@ $sectionName = str_replace(' ', '', strtolower($title));
                     echo $address ? "<div class='contact-us-text' style='color:white; padding-bottom:20px;'>{$address}</div>" : '';
                     ?>
                     <div>
-                        <a href="https://www.linkedin.com/company/ergiuk/"><img src="wp-content/themes/brando/assets/images/linkedin.png" title="Twitter link" width="30" height="30"></a>
+                        <a href="https://www.linkedin.com/company/ergiuk/" target="_blank"><img src="wp-content/themes/brando/assets/images/linkedin.png" title="Twitter link" width="30" height="30"></a>
                         <a style="padding-right:15px; display:none;"href="#home"><img src="wp-content/themes/brando/assets/images/twitter.png" title="Twitter link" width="30" height="30"></a>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 contact-form" style="margin-top:10px; padding-left:0px;"" >
+            <div class="col-lg-4 contact-form" style="margin-top:10px; padding-left:0px;" >
             <div class="footerlink-title footermarginb">Make an inquiry</div>
                 <div style="padding-left:15px;"><?php echo do_shortcode('[contact-form-7 id="11" title="Contact Form"]') ?></div>
             </div>
