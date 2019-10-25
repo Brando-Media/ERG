bbed8726b75b11d7a6bb2ccc13781d58c73b3113
df165 <davidbd@live.com>
2019-10-25 10:29:05 +0100

    main image lightbox

--- wp-content/themes/brando/templates/project/images.php
+++ wp-content/themes/brando/templates/project/images.php
@@ -30,7 +30,7 @@ $mainimage = get_sub_field('main_image');?>
         <div class="col-md-6 container mw30img" style="padding-left:0px!important; padding-right:0px!important; margin-right:0px; margin-left:0px;" >
         <a >
                                 <div class="casestudyhover" style="position:relative;">
-                                    <a href="#">
+                                    <a href="<?php echo $mainimage['url'] ?>" data-rel="lightbox">
                                     <img class="img-fluid w-100 project-home-main-image casestudymainimage" style="height:100%;"src="<?php echo show_image($mainimage['url'], $mainimage['width'], $mainimage['height'], 75) ?>">
                                         <div class="overlay"> 
                                             <div class="viewcasestudy">Click to expand</div>
@@ -58,7 +58,7 @@ $mainimage = get_sub_field('main_image');?>
            <div class="<?php echo $class ?>" style="min-width:23vw;">
            <div class="<?php echo $class ?> col-md-12 container  casestudy-div-img maxwidth23 from50to100p <?php echo $class ?>" style="padding-left:0px!important; padding-right:0px!important; margin-left:0px!important; margin-right:0px!important; width:100%;" >
                                 <div class="casestudyhover" style="position:relative;">
-                                    <a href="#">
+                                    <a href="<?php echo $image['url'] ?>" data-rel="lightbox">
                                     <img class="small-project-homep maxwidth23 from50to100p" src="<?php echo show_image($image['url'], $image['width'], $image['height'], 75) ?>">
                                         <div class="overlay"> 
                                             <div class="viewcasestudy">Click to expand</div>
