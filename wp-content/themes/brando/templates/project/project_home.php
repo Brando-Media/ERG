84f0862270ac2c8762fe95d4a4254668bcde5319
df165 <davidbd@live.com>
2019-10-28 14:41:33 +0000

    css project

--- wp-content/themes/brando/templates/project/project_home.php
+++ wp-content/themes/brando/templates/project/project_home.php
@@ -17,7 +17,7 @@ $next_url = get_sub_field('nexturl');
             <div class="col-lg-8 text-center left-content-work-block pt200m100" style=" padding-left:50px; padding-right:70px; /* max-width:85%; padding-left:35px;*/">
                 <div class="project-home-image-75vw indicasestudyhideimage" style="margin:0px; float:right;">
                     <?php if($image): ?>
-                        <img class="img-fluid img-phome project75vwimage" src="<?php echo show_image($image['url'], $image['width'], $image['height']); ?>">
+                        <img class="img-fluid img-phome project59vwimage" src="<?php echo show_image($image['url'], $image['width'], $image['height']); ?>">
                     <?php endif; ?>
                 </div>
                 
