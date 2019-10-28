e8b7458144f80d192f52121fa8df0e452018c20e
df165 <davidbd@live.com>
2019-10-28 15:47:34 +0000

    reducing the top

--- wp-content/themes/brando/footer-casestudies.php
+++ wp-content/themes/brando/footer-casestudies.php
@@ -1,11 +1,11 @@
-<div style="max-width:5700px;" id="brando-container" class="container"><a  href="#top"><span class="scrollererg casestudiesfooter" style="text-shadow: 0 0 3px #000000, 0 0 0!important; position:absolute; top: 120px;" id="brando-red">ERG INTERNATIONAL GROUP</span></a></div> 
+<div style="max-width:5700px;" id="brando-container" class="container"><a  href="#top"><span class="scrollererg casestudiesfooter" style="text-shadow: 0 0 3px #000000, 0 0 0!important; position:absolute; top: 50px;" id="brando-red">ERG INTERNATIONAL GROUP</span></a></div> 
             <footer id="footer" class="container">
                 <!--<a href="#" class="float-right">Instagram</a>-->
             </footer>
         </div><?php //#wrapper ?>
         <?php wp_footer(); ?>
         <script src="<?php bloginfo('template_directory'); ?>/javascript/script.js"></script>
-
+ 
         <script>
             <?php if(!is_singular('work')): ?>
                 $(window).on('load', function(){
