0b84cdb56001a5c17c3d7afd91ecb06a13b5815f
df165 <davidbd@live.com>
2019-10-28 14:08:02 +0000

    Update imagessolo.php

--- wp-content/themes/brando/templates/project/imagessolo.php
+++ wp-content/themes/brando/templates/project/imagessolo.php
@@ -11,7 +11,7 @@ $images = get_sub_field('images');
            <div class="<?php echo $class ?> col-md-12 container  casestudy-div-img maxwidth23 from50to100p <?php echo $class ?>" style="padding-left:0px!important; padding-right:0px!important; margin-left:0px!important; margin-right:0px!important; width:100%;" >
                                 <div class="casestudyhover" style="position:relative;">
                                     <a href="<?php echo $image['url'] ?>" data-rel="lightbox">
-                                    <img style="max-width:100vw!important;" class="maxwidth23 from50to100p" src="<?php echo show_image($image['url'], $image['width'], $image['height'], 75) ?>">
+                                    <img class="maxwidth23 from50to100p" src="<?php echo show_image($image['url'], $image['width'], $image['height'], 75) ?>">
                                         <div class="overlay"> 
                                             <div class="viewcasestudy">Click to expand</div>
                                         </div>
