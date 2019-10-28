2f32b62a5fef41fc8c9e97ff62d0773a6b89a196
df165 <davidbd@live.com>
2019-10-28 17:05:56 +0000

    css

--- wp-content/themes/brando/templates/project/project_home.php
+++ wp-content/themes/brando/templates/project/project_home.php
@@ -35,7 +35,7 @@ $next_url = get_sub_field('nexturl');
 <div class="casestudyarrows" style="">
 <div id="wrapper" class="<?php echo $class; ?> ">
         <header id="" class="arrowposproject" style="">
-            <div id="top" class="container clearfix header-toggle-white header-div" style="margin-left:60px; max-width:2400px; padding-left:0px; padding-right:0px;">
+            <div id="top" class="container clearfix header-toggle-white header-div casestudiesmarginleft" style="margin-left:60px; max-width:2400px; padding-left:0px; padding-right:0px;">
                 <a href="#home" id="logo" class="float-left" style="padding-left:15px;">    
                         <a class="leftarrowstyle" style="font-family: Helvetica;color: #00498D!important; text-align: left;" href="<?php echo $previous_url ?>"> <img class="leftarrowmargin" src="../wp-content/themes/brando/assets/images/rightah.png"><?php echo $previoustext ?></a>
                 </a>
