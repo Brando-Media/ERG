b3c0313af59fda020e3130674eb98d850d9ff045
df165 <davidbd@live.com>
2019-11-01 17:36:42 +0000

    triple css

--- wp-content/themes/brando/templates/project/imagesflex.php
+++ wp-content/themes/brando/templates/project/imagesflex.php
@@ -64,10 +64,10 @@ $noofimg = count($images);
         <div class="row m-0">
         <?php foreach($images as $k=>$image): ?>
            <div class="<?php echo $class ?>" style="min-width:33.333vw;">
-           <div class="<?php echo $class ?> col-md-12 container  casestudy-div-img maxwidth23  hfcproj from50to100p33 <?php echo $class ?>" style="padding-left:0px!important; padding-right:0px!important; margin-left:0px!important; margin-right:0px!important; width:100%;" >
+           <div class="<?php echo $class ?> heighttriple col-md-12 container  casestudy-div-img maxwidth23  hfcproj from50to100p33 <?php echo $class ?>" style="padding-left:0px!important; padding-right:0px!important; margin-left:0px!important; margin-right:0px!important; width:100%;" >
                             <div class="casestudyhover" style="position:relative;">
                                     <a href="<?php echo $image['url'] ?>" data-rel="lightbox">
-                                    <img class="maxwidth23 hfcproj from50to100p33" src="<?php echo show_image($image['url'], $image['width'], $image['height'], 75) ?>">
+                                    <img class="maxwidth23 heighttriple hfcproj from50to100p33" src="<?php echo show_image($image['url'], $image['width'], $image['height'], 75) ?>">
                                         <div class="overlay"> 
                                             <div class="viewcasestudy">Click to expand</div>
                                         </div>
