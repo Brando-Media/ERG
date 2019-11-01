1e10477114c859bd37819b90538316595c0de238
df165 <davidbd@live.com>
2019-11-01 14:07:56 +0000

    four erg

--- wp-content/themes/brando/templates/project/imagesflex.php
+++ wp-content/themes/brando/templates/project/imagesflex.php
@@ -188,6 +188,28 @@ padding-right: 0px;">
 
     <?php }else if($noofimg =="6"){ ?>
 
+        <?php if($images): ?>
+    <div class="row m-0 posabs991">
+        <div class="col-md-12 mw69div bottomimagestriple" style="padding-left:0px!important; padding-right:0px!important;">
+        <div class="row m-0">
+        <?php foreach($images as $k=>$image): ?>
+           <div class="<?php echo $class ?>" style="min-width:50vw;">
+           <div class="<?php echo $class ?> col-md-12 container  casestudy-div-img maxwidth23 from50to100p <?php echo $class ?>" style="padding-left:0px!important; padding-right:0px!important; margin-left:0px!important; margin-right:0px!important; width:100%;" >
+                                <div class="casestudyhover" style="position:relative;">
+                                    <a href="<?php echo $image['url'] ?>" data-rel="lightbox">
+                                    <img class="maxwidth23 from50to100p" src="<?php echo show_image($image['url'], $image['width'], $image['height'], 75) ?>">
+                                        <div class="overlay"> 
+                                            <div class="viewcasestudy">Click to expand</div>
+                                        </div>
+                                    </a>
+                                </div>
+            </div>
+            </div>
+        <?php endforeach; ?>
+            </div>
+            </div>
+<?php endif; ?>
+
     <?php }else if($noofimg =="7"){ ?>
 
         <?php if($images): ?>
