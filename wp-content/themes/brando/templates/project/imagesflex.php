cfa7ce7ca0b23cade4eeae4e615a20235cd257d0
Illya <davidbd@live.com>
2019-11-03 22:00:03 +0000

    Update imagesflex.php

--- wp-content/themes/brando/templates/project/imagesflex.php
+++ wp-content/themes/brando/templates/project/imagesflex.php
@@ -67,7 +67,7 @@ $noofimg = count($images);
            <div class="<?php echo $class ?> heighttriple col-md-12 container  casestudy-div-img maxwidth23  hfcproj from50to100p33 <?php echo $class ?>" style="padding-left:0px!important; padding-right:0px!important; margin-left:0px!important; margin-right:0px!important; width:100%;" >
                             <div class="casestudyhover" style="position:relative;">
                                     <a href="<?php echo $image['url'] ?>" data-rel="lightbox">
-                                    <img class="maxwidth23 heighttriple hfcproj from50to100p33" src="<?php echo show_image($image['url'], $image['width'], $image['height'], 75) ?>">
+                                    <img class="maxwidth23 heighttriple hfcproj from50to100p33" src="<?php echo show_image($image['url'], $images[0]['width'], $images[0]['height'], 75) ?>">
                                         <div class="overlay"> 
                                             <div class="viewcasestudy">Click to expand</div>
                                         </div>
