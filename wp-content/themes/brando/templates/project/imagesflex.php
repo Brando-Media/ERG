c7feb1b02a33c0d183be7919e27802aba55777d4
Illya <davidbd@live.com>
2019-11-03 18:37:10 +0000

    css

--- wp-content/themes/brando/templates/project/imagesflex.php
+++ wp-content/themes/brando/templates/project/imagesflex.php
@@ -91,7 +91,7 @@ $noofimg = count($images);
            <div class="<?php echo $class ?> col-md-12 container  casestudy-div-img maxwidth23 hfcproj from50to100p <?php echo $class ?>" style="padding-left:0px!important; padding-right:0px!important; margin-left:0px!important; margin-right:0px!important; width:100%;" >
                                 <div class="casestudyhover" style="position:relative;">
                                     <a href="<?php echo $image['url'] ?>" data-rel="lightbox">
-                                    <img class="maxwidth23 hfcproj from50to100p" src="<?php echo show_image($image['url'], $image['width'], $image['height'], 75) ?>">
+                                    <img class="maxwidth23 hfcproj from50to100p" src="<?php echo show_image($image['url'], $images[0]['width'], $images[0]['height'], 75) ?>">
                                         <div class="overlay"> 
                                             <div class="viewcasestudy">Click to expand</div>
                                         </div>
@@ -165,7 +165,7 @@ padding-right: 0px;">
         if($k>4){
         $class = 'hideonmobilelate'; ?>
        <?php } ?>
-       <div class="<?php echo $class ?>" style="min-width:33.333%;">
+       <div class="<?php echo $class ?> fiveimages" style="min-width:33.333%;">
        <div class="<?php echo $class ?> col-md-12 container  casestudy-div-img maxwidth23 hfcproj from50to100p <?php echo $class ?>" style="padding-left:0px!important; padding-right:0px!important; margin-left:0px!important; margin-right:0px!important; width:100%;" >
                             <div class="casestudyhover" style="position:relative;">
                                 <a href="<?php echo $image['url'] ?>" data-rel="lightbox">
