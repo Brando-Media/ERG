b1d98112c25fd7c872979b40df0420783939666e
df165 <davidbd@live.com>
2020-02-03 12:53:44 +0000

    Revert "removing conteng-image from our focus"
    This reverts commit 2ec2b632437bd56c0fd9b993e2269087f3d5a99d.

--- wp-content/themes/brando/templates/ourfocus/project_filter.php
+++ wp-content/themes/brando/templates/ourfocus/project_filter.php
@@ -44,7 +44,7 @@ $first = 0;
                 </div>
                 <div class="projects-home col-lg-6 text-center /*align-self-center*/ left-content-work-block pfcol6" style="padding-left:0px; padding-right:0px;">
                 <?php if($image): ?>
-                    <img class="img-fluid mt55of" src="<?php echo show_image($image['url'], $image['width'], $image['height']); ?>">
+                    <img class="img-fluid content-image mt55of" src="<?php echo show_image($image['url'], $image['width'], $image['height']); ?>">
                 <?php endif; ?>
             </div>
             </div>
